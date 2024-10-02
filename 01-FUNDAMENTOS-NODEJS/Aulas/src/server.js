cd88bbcdd7743756e0e1d390416bcce0f3e4fc09
Vinicius Albuquerque de Carvalho <vinicius.10.carvalho.100@gmail.com>
2024-10-02 18:43:47 -0300

    AULAS 01-FUNDAMENTOS

---
+++ 01-FUNDAMENTOS-NODEJS/Aulas/src/server.js
@@ -0,0 +1,33 @@
+import http from 'node:http'
+
+import { json } from './middlewares/json.js'
+import { routes } from './routes.js'
+import { extractQueryParams } from './utils/extract-query-params.js'
+
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req
+
+  await json(req, res)
+
+  const route = routes.find((route) => {
+    return route.method === method && route.path.test(url)
+  })
+
+  if (route) {
+    const routParams = req.url.match(route.path)
+
+    // console.log(extractQueryParams(routParams.groups.query));
+
+    const { query, ...params } = routParams.groups
+
+    req.params = params
+    req.query = query ? extractQueryParams(query) : {}
+
+    return route.handler(req, res)
+  }
+
+  return res.writeHead(404).end()
+})
+
+console.log('HTTP server running!');
+server.listen(3333)
