29f553ebe2789a538e86e4c957c1ee2dc076f00d
Vinicius Albuquerque de Carvalho <vinicius.10.carvalho.100@gmail.com>
2024-10-03 23:52:25 -0300

    aula 2 parte 1 e 2

---
+++ 02-API-REST-NODEJS/Aulas/src/server.ts
@@ -0,0 +1,18 @@
+import fastify from 'fastify'
+
+import { knex } from './database'
+import { env } from './env'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  const transaction = await knex('transactions')
+    .select('*')
+    .where('amount', 1000)
+
+  return transaction
+})
+
+app.listen({ port: env.PORT }).then(() => {
+  console.log('Server listening on port 3333')
+})
