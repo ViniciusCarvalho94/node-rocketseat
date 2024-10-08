77568c62ced1920944913b6d5ec73300ddfdb192
Vinicius Albuquerque de Carvalho <vinicius.10.carvalho.100@gmail.com>
2024-10-07 21:03:38 -0300

    aula 2 parte 3

--- 02-API-REST-NODEJS/Aulas/src/server.ts
+++ 02-API-REST-NODEJS/Aulas/src/server.ts
@@ -1,16 +1,14 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
 
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transaction = await knex('transactions')
-    .select('*')
-    .where('amount', 1000)
-
-  return transaction
+app.register(cookie)
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app.listen({ port: Number(env.PORT) }).then(() => {
