72046eb696b7bfa6d7a99464719481864991d2ad
Vinicius Albuquerque de Carvalho <vinicius.10.carvalho.100@gmail.com>
2024-10-26 03:31:59 -0300

    Aula nivel 3 modulo 7

---
+++ 03-SOLID-NODEJS/Aulas/src/http/controllers/users/refresh.ts
@@ -0,0 +1,38 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+
+export async function refresh(request: FastifyRequest, reply: FastifyReply) {
+  await request.jwtVerify({ onlyCookie: true })
+
+  const { role } = request.user
+
+  const token = await reply.jwtSign(
+    { role },
+    {
+      sign: {
+        sub: request.user.sub,
+      },
+    },
+  )
+
+  const refreshTtoken = await reply.jwtSign(
+    { role },
+    {
+      sign: {
+        sub: request.user.sub,
+        expiresIn: '7d',
+      },
+    },
+  )
+
+  return reply
+    .setCookie('refreshToken', refreshTtoken, {
+      path: '/',
+      secure: true,
+      sameSite: true,
+      httpOnly: true,
+    })
+    .status(200)
+    .send({
+      token,
+    })
+}
--- 03-SOLID-NODEJS/Aulas/src/http/controllers/users/routes.ts
+++ 03-SOLID-NODEJS/Aulas/src/http/controllers/users/routes.ts
@@ -3,10 +3,13 @@ import { register } from '../users/register'
 import { authenticate } from '../users/authenticate'
 import { profile } from '../users/profile'
 import { verifyJWT } from '../../middlewares/verify-jwt'
+import { refresh } from './refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   /** Authenticated **/
   app.get('/me', { onRequest: [verifyJWT] }, profile)
